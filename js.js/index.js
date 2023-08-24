4b44c4b84f6b99d79a4241a3c8acd01a5f678a22
Gabriel Oquendo <oquendogabriel18@gamil.com>
2023-08-24 16:59:31 -0500

    Responsive

---
+++ js.js/index.js
@@ -0,0 +1,7 @@
+// const boton = document.getElementById("boton");
+
+// boton.addEventListener("click", function () {
+//   const classList = document.getElementById("nav").classList;
+//   classList.toggle("hidden");
+//   classList.toggle("block");
+// });
